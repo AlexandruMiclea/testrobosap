d754094c5e007c65e8968aa1a215ceb1c80c9075
Negoitescu Miruna <miruru2003@gmail.com>
2021-02-14 14:37:50 +0200

    i did? a thing?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/localizer/AnalogEncoderLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/localizer/AnalogEncoderLocalizer.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class AnalogEncoderLocalizer extends TwoTrackingWheelLocalizer {
     public static double TICKS_PER_REV = 4175; //TODO
-    public static double WHEEL_RADIUS = 2; // inch
+    public static double WHEEL_RADIUS = 6; // inch
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double ENCODER_RATIO = 1.0; // ratio between left encoder ticks per rev and right encoder ticks per rev
@@ -24,9 +24,12 @@ public class AnalogEncoderLocalizer extends TwoTrackingWheelLocalizer {
     public static double LATERAL_DISTANCE = 13; // inch; distance between the left and right wheels
     public static double FORWARD_OFFSET = 0; // inch; offset of the lateral wheel
 
-    private AnalogSensor rightEncoder, middleEncoder;
-    private double middleTicks, rightTicks;
-    private double lastMiddleTicks, lastRightTicks;
+//    private AnalogSensor rightEncoder, middleEncoder;
+//    private double rightPosition, middlePosition; //volts
+//    private double lastMiddlePos, lastRightPos; //volts
+
+    private  absoluteEncoder rightEncoder = new absoluteEncoder();
+    private absoluteEncoder middleEncoder = new absoluteEncoder();
     BNO055IMU imu;
 
     public AnalogEncoderLocalizer(HardwareMap hardwareMap) {
@@ -35,8 +38,8 @@ public class AnalogEncoderLocalizer extends TwoTrackingWheelLocalizer {
                 new Pose2d(FORWARD_OFFSET, 0, Math.toRadians(90)) // front
         ));
 
-        rightEncoder = (AnalogSensor) hardwareMap.get("rightEncoder");
-        middleEncoder = (AnalogSensor) hardwareMap.get("MiddleEncoder");
+        rightEncoder.encoder = (AnalogSensor) hardwareMap.get("rightEncoder");
+        middleEncoder.encoder = (AnalogSensor) hardwareMap.get("middleEncoder");
 
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -44,26 +47,53 @@ public class AnalogEncoderLocalizer extends TwoTrackingWheelLocalizer {
         imu.initialize(parameters);
     }
 
-
- //FUNCTIE GET POSITION
-
-    public static double encoderTicksToInches(int ticks) {
-        return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+    public static double voltageToInches(double pos) {
+        return ((pos * WHEEL_RADIUS * 3.14) / 5);
     }
 
     @NonNull
     @Override
     public List<Double> getWheelPositions() {
         return Arrays.asList(
-                //si nu o sa fie ticks to inches, ci o sa fie positon???? sau ce om pune to inches
-//                encoderTicksToInches(functie get position right encoder),
-//                encoderTicksToInches(functie get position middle encoder)
+                voltageToInches(rightEncoder.getTotalVoltage()),
+                voltageToInches(middleEncoder.getTotalVoltage())
         );
     }
 
     @Override
     public double getHeading() {
         return imu.getAngularOrientation().firstAngle;
+    }
+}
+
+class absoluteEncoder{
+    public AnalogSensor encoder;
+    public double lastVoltage;
+    public double totalVoltage;
+
+//    absoluteEncoder (AnalogSensor encoder, double lastVoltage, double totalVoltage){
+//        this.encoder = encoder;
+//        this.lastVoltage = lastVoltage;
+//        this.totalVoltage = totalVoltage;
+//    }
+
+    public double getTotalVoltage(){
+        double current = encoder.readRawVoltage();
+
+        double derivative = current - lastVoltage;
+
+        if (derivative < 2.5){
+            derivative +=5;
+        } else if (derivative > 2.5){
+            derivative -=5;
+        }
+
+        lastVoltage = current;
+
+        totalVoltage +=derivative;
+
+        return totalVoltage;
+
     }
 }
 
