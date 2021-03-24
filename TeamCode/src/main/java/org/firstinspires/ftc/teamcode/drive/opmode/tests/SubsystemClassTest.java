80c24a77c13db55362903d0def4a0ed5fcc63daf
Negoitescu Miruna <miruru2003@gmail.com>
2021-03-24 09:43:57 +0200

    WobbleArm test class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/tests/SubsystemClassTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.tank.Robot;
+
+@Config
+@Autonomous(group = "test", name = "Test Wobble")
+public class SubsystemClassTest extends LinearOpMode {
+
+    public Robot robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData(">", "Initializing...");
+        telemetry.update();
+        robot = new Robot(hardwareMap);
+        robot.wobbleArm.clawToggle(true);
+        robot.wobbleArm.armPositionToggle(true);
+
+        while (robot.isInitialize() && opModeIsActive()) {
+            idle();
+        }
+
+        telemetry.addData(">", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        robot.wobbleArm.armPositionToggle(false);
+        robot.wobbleArm.clawToggle(false);
+        robot.wobbleArm.clawToggle(true);
+        robot.wobbleArm.clawToggle(false);
+        robot.wobbleArm.armPositionToggle(true);
+
+    }
+}
